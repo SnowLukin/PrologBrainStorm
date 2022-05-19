235890848e443c92fa231ab7671f508ad6b0705d
Snow Lukin <everyoursden@gmail.com>
2022-05-19 21:03:36 +0300

    Updated permutation term

--- 2.pl
+++ 2.pl
@@ -101,30 +101,29 @@ writeList([Head|Tail]):- write(Head), write(' '), writeList(Tail).
 
 partialPermutation(List, PermutationLength):-
     partialPermutation(List, PermutationLength, []).
-partialPermutation(_, 0, TempList):-
-    writeList(TempList), !, fail.
-partialPermutation(List, PermutationLength, TempList):-
+partialPermutation(_, 0, ResultList):-
+    writeList(ResultList), !, fail.
+partialPermutation(List, PermutationLength, ResultList):-
     select(Head, List, ListTail),
     NewPermutationLength is PermutationLength - 1,
-    append(TempList, [Head], NewTempList),
-    partialPermutation(ListTail, NewPermutationLength, NewTempList).
+    append(ResultList, [Head], NewResultList),
+    partialPermutation(ListTail, NewPermutationLength, NewResultList).
 
 /*
     Example:
-        ?- permutation([1,2,3],X).
-        X = [1, 2, 3] ;
-        X = [1, 3, 2] ;
-        X = [2, 1, 3] ;
-        X = [2, 3, 1] ;
-        X = [3, 1, 2] ;
-        X = [3, 2, 1].
+        ?- permutation([1,2,3]).
+        1 2 3
+        1 3 2
+        2 1 3
+        2 3 1
+        3 1 2
+        3 2 1
 */
-% There is builtIn function for permutation
-/*
-permutation(List, Permutation):-
+
+permutation(List):-
     length(List, Length),
-    partialPermutation(List, Length, Permutation).
-*/
+    partialPermutation(List, Length).
+
 
 /*
     Example:
